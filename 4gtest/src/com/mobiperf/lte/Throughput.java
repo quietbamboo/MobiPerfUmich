9e547ea37db06393ccd62fcf701cc238692b5270
Junxian Huang <junxian.huang@gmail.com>
2011-10-05 21:59:35 -0400

    clear up

--- 4gtest/src/com/mobiperf/lte/Throughput.java
+++ 4gtest/src/com/mobiperf/lte/Throughput.java
@@ -24,275 +24,275 @@ import android.util.Log;
 
 
 class Throughput  {
-	
+
 	public static long uplinkSize;
 	public static long uplinkTime;
-	
+
 	public static int MeasureUplinkTput( String serverIP,int serverPort ) {
-		
+
 		//Junxian: New, ask the server to start tcpdump now to collect 3 way handshake
 		(new Report()).sendReport(Definition.COMMAND_TCP_UPLINK);
-		
+
 		//sleep for a while waiting the server to be ready
 		try {
 			Thread.sleep(4000);
 		} catch (InterruptedException e4) {
 			e4.printStackTrace();
 		}
-		
-		
+
+
 		uplinkSize = -1;
-		
-		
+
+
 		Socket tcpSocket = null;
 		DataOutputStream os = null;
 		DataInputStream is = null;
-		
+
 		try {
-		    tcpSocket = new Socket();
-		    SocketAddress remoteAddr = new InetSocketAddress( serverIP, serverPort );
-		    tcpSocket.connect( remoteAddr, Definition.TCP_TIMEOUT_IN_MILLI );
-		    os = new DataOutputStream( tcpSocket.getOutputStream() );
-		    is = new DataInputStream( tcpSocket.getInputStream() );
-		    tcpSocket.setSoTimeout( Definition.TCP_TIMEOUT_IN_MILLI );
-		    tcpSocket.setTcpNoDelay( true );
-		
-		
+			tcpSocket = new Socket();
+			SocketAddress remoteAddr = new InetSocketAddress( serverIP, serverPort );
+			tcpSocket.connect( remoteAddr, Definition.TCP_TIMEOUT_IN_MILLI );
+			os = new DataOutputStream( tcpSocket.getOutputStream() );
+			is = new DataInputStream( tcpSocket.getInputStream() );
+			tcpSocket.setSoTimeout( Definition.TCP_TIMEOUT_IN_MILLI );
+			tcpSocket.setTcpNoDelay( true );
+
+
 		}
 		catch ( UnknownHostException e ) {
-		    return 1;
+			return 1;
 		}
 		catch ( Exception e ) {
-		    //TODO
-		    return 2;
+			//TODO
+			return 2;
 		}
-		
+
 		if ( tcpSocket == null || os == null || is == null ) {
-		    //TODO
-		            return 3;
-		        }
-		
-		        int retval = -1;
+			//TODO
+			return 3;
+		}
+
+		int retval = -1;
 		outer: {
-		            if ( Utilities.checkStop() ) {
-		                break outer;
-		            }
-		
-		            long count = 0;
-		
-		            String buf = "ehcsinmfuevrxruudycdbgdyrlmbapvxpbmzshhkrtewlijrlzbcfobxxugvnjqehdpnsiuwqnbhshrgfgivdyafowedzjlxwxavfsbvzqdddaiorpnpmunqzihrgewqebvnymvujrylymgwliovsyyoozkdgcskedzmhxijruaaurpocsnxtitlcvotrxpvnzwornmoicpaxbobfoehwvirpannjeizbtkizvdgmhgjjkljmjkculysjdvfnsranueaizstwrtuszgfknbsarwkfsrcuhjvzhvcduabphnusscfvqyqpfyndbplpklrwqrpgyitigaeowfnxnfvysdrwjpvbustrltyoqrtunmnxxenmyudvatlevpzsqmfwlzdsglthvwfvldylyktapinzkztygsbzfnbeiimstfjgppamkimryjnxmojdiezuhkvjzgqrfcmhrgcaqyqktvsdxnyptamfmsvghunxbeqlydmnkeqgzgdjjyemgmgxrlsczsuzenyeozgvhhrdawzgvgjueaykkcqlswfcjozucztcyynorcarkhsbgmzodkxjbdejbtxldpaoapyithrskisxyrrcrbuaezveueikvppwzvyvloytphbztcumodlhmvcwdqwtgtnnmlnhmdvpsrnfbbzydikyvamnzxudoeppvhonysvzjccfatxyosaumvgkxdpwsjbtpqcscfyqzruztafodqhfywacsqocckdlssrpnvoycecwvzzsyzbwmnkfpvupudfhrocunyzpytdtvznuskauhaancoylvcezzbgnrayvhwxjocckahppqhotpoccserezellvwijjdqfakcvjknxnjnibdyugxfpsnsrgxmkgbsjyynrdfdifcrxvgcyvtbseipkxhlajjpsmoqjdijeoudfvqpfjwjixfzgdhnkhyahdiuezbpxyjqhblahgwyqosjjqcdbvbqdabrxgmirbtv";
-		
-		    byte [] message =  InformationCenter.getPrefix().getBytes();
-		    
-		    Log.v("LOG", "sent prefix for uplink " + InformationCenter.getPrefix());
-		    
-		    
-		
-		    try {
-		        os.write( message );
-		        os.flush();
-		    }
-		    catch ( Exception e3 ) {
-		        // TODO Auto-generated catch block
-		        retval = 4;
-		        break outer;
-		    }
-		
-		    
-		    //don't need to check response
-		    try {
+			if ( Utilities.checkStop() ) {
+				break outer;
+			}
+
+			long count = 0;
+
+			String buf = "ehcsinmfuevrxruudycdbgdyrlmbapvxpbmzshhkrtewlijrlzbcfobxxugvnjqehdpnsiuwqnbhshrgfgivdyafowedzjlxwxavfsbvzqdddaiorpnpmunqzihrgewqebvnymvujrylymgwliovsyyoozkdgcskedzmhxijruaaurpocsnxtitlcvotrxpvnzwornmoicpaxbobfoehwvirpannjeizbtkizvdgmhgjjkljmjkculysjdvfnsranueaizstwrtuszgfknbsarwkfsrcuhjvzhvcduabphnusscfvqyqpfyndbplpklrwqrpgyitigaeowfnxnfvysdrwjpvbustrltyoqrtunmnxxenmyudvatlevpzsqmfwlzdsglthvwfvldylyktapinzkztygsbzfnbeiimstfjgppamkimryjnxmojdiezuhkvjzgqrfcmhrgcaqyqktvsdxnyptamfmsvghunxbeqlydmnkeqgzgdjjyemgmgxrlsczsuzenyeozgvhhrdawzgvgjueaykkcqlswfcjozucztcyynorcarkhsbgmzodkxjbdejbtxldpaoapyithrskisxyrrcrbuaezveueikvppwzvyvloytphbztcumodlhmvcwdqwtgtnnmlnhmdvpsrnfbbzydikyvamnzxudoeppvhonysvzjccfatxyosaumvgkxdpwsjbtpqcscfyqzruztafodqhfywacsqocckdlssrpnvoycecwvzzsyzbwmnkfpvupudfhrocunyzpytdtvznuskauhaancoylvcezzbgnrayvhwxjocckahppqhotpoccserezellvwijjdqfakcvjknxnjnibdyugxfpsnsrgxmkgbsjyynrdfdifcrxvgcyvtbseipkxhlajjpsmoqjdijeoudfvqpfjwjixfzgdhnkhyahdiuezbpxyjqhblahgwyqosjjqcdbvbqdabrxgmirbtv";
+
+			byte [] message =  InformationCenter.getPrefix().getBytes();
+
+			Log.v("LOG", "sent prefix for uplink " + InformationCenter.getPrefix());
+
+
+
+			try {
+				os.write( message );
+				os.flush();
+			}
+			catch ( Exception e3 ) {
+				// TODO Auto-generated catch block
+				retval = 4;
+				break outer;
+			}
+
+
+			//don't need to check response
+			try {
 				Thread.sleep(4000); //sleep for 4 seconds for server to be ready
 			} catch (InterruptedException e1) {
 				e1.printStackTrace();
 			}
-		
-		    message = buf.getBytes();
-		    System.out.println ("------- MESSAGE LENGTH = " + message.length);
-		    long startTime = System.currentTimeMillis();
-		    long endTime = System.currentTimeMillis();
-		    
-		    //Test lasts 16 seconds - Junxian
-		    do {
-		        if ( Utilities.checkStop() ) {
-		            break outer;
-		        }
-		
-		        count++;
-		
-		        try {
-		            os.write( message );
-		        }
-		        catch ( Exception e ) {
-		            // TODO Auto-generated catch block
-		            retval = 5;
-		            break outer;
-		        }
-		
-		        endTime = System.currentTimeMillis();
-		
-		
-		    }
-		    while ( ( endTime - startTime ) < Definition.TP_DURATION_IN_MILLI );
-		
-		    uplinkTime = endTime - startTime;
-		    uplinkSize = count * ( message.length );
+
+			message = buf.getBytes();
+			System.out.println ("------- MESSAGE LENGTH = " + message.length);
+			long startTime = System.currentTimeMillis();
+			long endTime = System.currentTimeMillis();
+
+			//Test lasts 16 seconds - Junxian
+			do {
+				if ( Utilities.checkStop() ) {
+					break outer;
+				}
+
+				count++;
+
+				try {
+					os.write( message );
+				}
+				catch ( Exception e ) {
+					// TODO Auto-generated catch block
+					retval = 5;
+					break outer;
+				}
+
+				endTime = System.currentTimeMillis();
+
+
+			}
+			while ( ( endTime - startTime ) < Definition.TP_DURATION_IN_MILLI );
+
+			uplinkTime = endTime - startTime;
+			uplinkSize = count * ( message.length );
 		}
-		
+
 		try {
 			os.close();
-		    is.close();
-		    tcpSocket.close();
-		
-		    if ( retval != -1 )
-		        return retval;
+			is.close();
+			tcpSocket.close();
+
+			if ( retval != -1 )
+				return retval;
 		}
 		catch ( Exception e2 ) {
-		    // TODO Auto-generated catch block
-		
-		        if ( retval != -1 )
-		            return retval;
-		        else
-		            return 6;
-		
-		    }
-		    return 7;
+			// TODO Auto-generated catch block
+
+			if ( retval != -1 )
+				return retval;
+			else
+				return 6;
+
+		}
+		return 7;
 	}
 
-     public static long downlinkSize;
-     public static long downlinkTime;
-     
-     public static int MeasureDownlinkTput( String serverIP,int serverPort ) {
-    	 
-    	//Junxian: New, ask the server to start tcpdump now to collect 3 way handshake
-    	 (new Report()).sendReport(Definition.COMMAND_TCP_DOWNLINK);
- 		
- 		//sleep for a while waiting the server to be ready
- 		try {
- 			Thread.sleep(4000);
- 		} catch (InterruptedException e4) {
- 			e4.printStackTrace();
- 		}
- 		
-         downlinkSize = -1;
-         downlinkTime = -1;
-         
-         Socket tcpSocket = null;
-         DataOutputStream os = null;
-         DataInputStream is = null;
+	public static long downlinkSize;
+	public static long downlinkTime;
 
-         try {
-             tcpSocket = new Socket();
-             SocketAddress remoteAddr = new InetSocketAddress( serverIP, serverPort );
-             tcpSocket.connect( remoteAddr, Definition.TCP_TIMEOUT_IN_MILLI );
-             tcpSocket.setSoTimeout( Definition.TCP_TIMEOUT_IN_MILLI );
-             tcpSocket.setTcpNoDelay( true );
+	public static int MeasureDownlinkTput( String serverIP,int serverPort ) {
 
-             os = new DataOutputStream( tcpSocket.getOutputStream() );
-             is = new DataInputStream( tcpSocket.getInputStream() );
-         }
-         catch ( UnknownHostException e ) {
-             return 1;
-         }
-         catch ( Exception e ) {
-             return 2;
-         }
+		//Junxian: New, ask the server to start tcpdump now to collect 3 way handshake
+		(new Report()).sendReport(Definition.COMMAND_TCP_DOWNLINK);
 
-         if ( tcpSocket == null || os == null || is == null ) {
-             return 3;
-         }
+		//sleep for a while waiting the server to be ready
+		try {
+			Thread.sleep(4000);
+		} catch (InterruptedException e4) {
+			e4.printStackTrace();
+		}
 
-         int retval = -1;
- outer: {
-             if ( Utilities.checkStop() ) {
-                 break outer;
-             }
+		downlinkSize = -1;
+		downlinkTime = -1;
 
-             // getting downlink data
-             byte [] message =  InformationCenter.getPrefix().getBytes();
+		Socket tcpSocket = null;
+		DataOutputStream os = null;
+		DataInputStream is = null;
 
-             try {
+		try {
+			tcpSocket = new Socket();
+			SocketAddress remoteAddr = new InetSocketAddress( serverIP, serverPort );
+			tcpSocket.connect( remoteAddr, Definition.TCP_TIMEOUT_IN_MILLI );
+			tcpSocket.setSoTimeout( Definition.TCP_TIMEOUT_IN_MILLI );
+			tcpSocket.setTcpNoDelay( true );
 
-                 os.write( message );
-                 os.flush();
+			os = new DataOutputStream( tcpSocket.getOutputStream() );
+			is = new DataInputStream( tcpSocket.getInputStream() );
+		}
+		catch ( UnknownHostException e ) {
+			return 1;
+		}
+		catch ( Exception e ) {
+			return 2;
+		}
 
-             }
-             catch ( Exception e3 ) {
-                 retval = 4;
-                 break outer;
-             }
-             
+		if ( tcpSocket == null || os == null || is == null ) {
+			return 3;
+		}
+
+		int retval = -1;
+		outer: {
+			if ( Utilities.checkStop() ) {
+				break outer;
+			}
+
+			// getting downlink data
+			byte [] message =  InformationCenter.getPrefix().getBytes();
+
+			try {
+
+				os.write( message );
+				os.flush();
+
+			}
+			catch ( Exception e3 ) {
+				retval = 4;
+				break outer;
+			}
+
 			//don't need to check server's response
- 		    try {
- 				Thread.sleep(2000); //sleep for 2 seconds for server to be ready
- 			} catch (InterruptedException e1) {
- 				e1.printStackTrace();
- 			}
+			try {
+				Thread.sleep(2000); //sleep for 2 seconds for server to be ready
+			} catch (InterruptedException e1) {
+				e1.printStackTrace();
+			}
 
-             if ( Utilities.checkStop() ) {
-                 break outer;
-             }
+			if ( Utilities.checkStop() ) {
+				break outer;
+			}
 
-             long startTime = System.currentTimeMillis();
-             long endTime = System.currentTimeMillis();
-             long total_read_bytes = 0;
+			long startTime = System.currentTimeMillis();
+			long endTime = System.currentTimeMillis();
+			long total_read_bytes = 0;
 
-             do {
-                 if ( Utilities.checkStop() ) {
-                     break outer;
-                 }
+			do {
+				if ( Utilities.checkStop() ) {
+					break outer;
+				}
 
-                 try {
-                     byte[] buffer = new byte[ 15000 ];
-
-
-                     int read_bytes = is.read( buffer, 0, 15000 );
-
-                     if ( read_bytes <= 0 )
-                         break;
-
-                     total_read_bytes += read_bytes;
+				try {
+					byte[] buffer = new byte[ 15000 ];
 
 
-                 }
-                 catch ( Exception e ) {
-                     e.printStackTrace();
-                     break;
-                 }
+					int read_bytes = is.read( buffer, 0, 15000 );
 
-                 endTime = System.currentTimeMillis();
-             }
-             while ( true );
+					if ( read_bytes <= 0 )
+						break;
 
-             if ( total_read_bytes > 1000 ) {
-             }
-             else {
-                 retval = 5;
-                 break outer;
-             }
+					total_read_bytes += read_bytes;
 
-             long timespent = ( endTime - startTime );
-             downlinkSize = total_read_bytes;
-             downlinkTime = timespent;
-         }
 
-         try {
-             os.close();
-             is.close();
-             tcpSocket.close();
+				}
+				catch ( Exception e ) {
+					e.printStackTrace();
+					break;
+				}
 
-             if ( retval != -1 )
-                 return retval;
-         }
-         catch ( Exception e2 ) {
-             if ( retval != -1 )
-                 return retval;
-             else
-                 return 6;
+				endTime = System.currentTimeMillis();
+			}
+			while ( true );
 
-         }
-         return 7;
+			if ( total_read_bytes > 1000 ) {
+			}
+			else {
+				retval = 5;
+				break outer;
+			}
 
-     }
+			long timespent = ( endTime - startTime );
+			downlinkSize = total_read_bytes;
+			downlinkTime = timespent;
+		}
 
-     
+		try {
+			os.close();
+			is.close();
+			tcpSocket.close();
+
+			if ( retval != -1 )
+				return retval;
+		}
+		catch ( Exception e2 ) {
+			if ( retval != -1 )
+				return retval;
+			else
+				return 6;
+
+		}
+		return 7;
+
+	}
+
+
 }
