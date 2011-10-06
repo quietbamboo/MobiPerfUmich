49837d017bb5de8a79c77afdbdaaedea9aaaa4e9
Junxian Huang <junxian.huang@gmail.com>
2011-10-06 14:18:58 -0400

    te

--- server/mlab.sh
+++ server/mlab.sh
@@ -18,8 +18,14 @@ if [ $1 = "-c" ]; then
 	cd ..
 
 elif [ $1 = "-d" ]; then
-	for n in `cat mlab/nodeList`
+	for n in `cat nodeList`
 	do
+	#	if [ $n = "mlab3.atl01.measurement-lab.org" ];then
+	#		echo "this one"
+	#	else
+	#		continue
+	#	fi
+
 		ping=`ping -c 2 -W 2 $n | grep " 0.0\% packet loss" | wc -l`
 		if [ $ping = "1" ]; then
 			echo $n " on"
@@ -29,14 +35,17 @@ elif [ $1 = "-d" ]; then
 		fi
 		echo "Deploy"
 		if [ $2 = "-e" ]; then
-			ssh -p 806 -l michigan_1 $n 'cd ~/mobiperf;bash end.sh'
+			ssh -o "StrictHostKeyChecking no" -p 806 -l michigan_1 $n 'cd ~/mobiperf;bash end.sh'
+		elif [ $2 = "-i" ]; then
+			ssh -o "StrictHostKeyChecking no" -p 806 -l michigan_1 $n 'sudo yum -y install java' &
 		else
-			ssh -p 806 -l michigan_1 $n 'mkdir ~/mobiperf'
-			scp -P 806  mlab/* michigan_1@$n:~/mobiperf
-			ssh -p 806 -l michigan_1 $n 'cd ~/mobiperf;bash start.sh'
+			#ssh -o "StrictHostKeyChecking no" -p 806 -l michigan_1 $n 'mkdir ~/mobiperf' &
+			scp -o "StrictHostKeyChecking no" -P 806  mlab/* michigan_1@$n:~/mobiperf
+			#ssh -o "StrictHostKeyChecking no" -p 806 -l michigan_1 $n 'cd ~/mobiperf;bash start.sh'
 		fi
-		exit
 	done
+elif [ $1 = "-t" ];then
+        ps aux | grep "measurement-lab.org" | awk '{system("sudo kill -9 " $2);}'
 else
-	echo "Usage: compile -c; deploy -d; terminate -d -e"
+	echo "Usage: compile -c; deploy -d; terminate remotely -d -e; install java -d -i; kill all local process -t"
 fi
