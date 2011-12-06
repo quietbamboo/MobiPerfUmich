08ecf97b1ae1bb5ab000d1e3260698cebd49f3c5
Junxian Huang <junxian.huang@gmail.com>
2011-12-05 23:57:36 -0500

    te

--- server/src/servers/KeepAlive.java
+++ server/src/servers/KeepAlive.java
@@ -10,7 +10,6 @@ package servers;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.net.InetAddress;
 import java.net.URL;
 import java.net.URLConnection;
 import java.net.URLEncoder;
